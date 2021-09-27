704a784cc5cfb2e999de75923a2229acf5d48976
Sergio Morales Esquivel <sergiome@gmail.com>
2021-09-26 21:54:09 -0600

    Changes to default data

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ API_KEY = os.environ['API_KEY']
 BASE_URL = os.environ['API_BASE_URL']
 
 default_name = "Kiki's Delivery Service"
-default_data = {"poster": "https://www.themoviedb.org/t/p/w500//7nO5DUMnGUuXrA4r2h6ESOKQRrx.jpg", "primaryColor": "#66A1D7", "otherColors": [{"color": "#293E7B", "proportion": 0.31275349256421814}, {"color": "#C5D5E5", "proportion": 0.16223524109959442}, {"color": "#8BB9E2", "proportion": 0.15998197386210006}, {"color": "#365291", "proportion": 0.08877872915727805}, {"color": "#5B78A2", "proportion": 0.08021631365479946}, {"color": "#95A3B4", "proportion": 0.07300585849481749}, {"color": "#26242A", "proportion": 0.05497972059486255}, {"color": "#736261", "proportion": 0.03830554303740424}, {"color": "#BFA47C", "proportion": 0.029743127534925643}]}
+default_data = {"poster": "https://www.themoviedb.org/t/p/w500//7nO5DUMnGUuXrA4r2h6ESOKQRrx.jpg", "primaryColor": "#6BA2D5", "otherColors": [{"color": "#2B4282", "proportion": 0.41303306181121224}, {"color": "#96C0E5", "proportion": 0.16626736942980355}, {"color": "#C8D5E2", "proportion": 0.15572592237661714}, {"color": "#607291", "proportion": 0.1317680881648299}, {"color": "#2E2A31", "proportion": 0.06851940584571155}, {"color": "#BAAC93", "proportion": 0.06468615237182558}]}
 
 @app.route("/")
 def index():
